a157e22c19127d07e005e9f9e61e69f505e1f095
monk3yd <jegajardog@gmail.com>
2022-07-23 21:50:09 -0300

    Refactor, can't find bug.

--- CS50p/pset5/test_plates/plates.py
+++ CS50p/pset5/test_plates/plates.py
@@ -1,9 +1,15 @@
+import sys
+
+
 def main():
     plate = input("Plate: ")
-    if is_valid(plate):
+    response = is_valid(plate)
+    if response:
         print("Valid")
-    else:
-        print("Invalid")
+        sys.exit(0)
+
+    print("Invalid")
+    sys.exit(1)
 
 
 def is_valid(string):
@@ -13,9 +19,13 @@ def is_valid(string):
 
     # Check for start with 2 letters
     invalid_start_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    valid_start_chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     first_letter = string[0]
     second_letter = string[1]
-    if first_letter in invalid_start_chars or second_letter in invalid_start_chars:
+    # if first_letter in invalid_start_chars or second_letter in invalid_start_chars:
+    #     return False
+
+    if first_letter not in valid_start_chars or second_letter not in valid_start_chars:
         return False
 
     try:
--- CS50p/pset5/test_plates/test_plates.py
+++ CS50p/pset5/test_plates/test_plates.py
@@ -1,17 +1,10 @@
 from plates import is_valid
 
 
-def main():
-    test_start()
-    test_nums()
-    test_length()
-    test_first_num()
-    test_punctuation()
-
-
 def test_start():
     assert is_valid("AZBB32") is True
     assert is_valid("4ZBB32") is False
+    assert is_valid("45BB32") is False
 
 
 def test_nums():
@@ -34,5 +27,7 @@ def test_punctuation():
     assert is_valid("BB.20") is False
 
 
-if __name__ == "__main__":
-    main()
+def test_non_alpha_chars():
+    assert is_valid("&%BB32") is False
+    assert is_valid("@*BB32") is False
+    assert is_valid("^HBB32") is False
