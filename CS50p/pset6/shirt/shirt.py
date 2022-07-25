2c615f46d301bc9b8e09538995b30e46cf77f1e6
monk3yd <jegajardog@gmail.com>
2022-07-25 00:08:50 -0300

    Finished shirt problem set for real.

--- CS50p/pset6/shirt/shirt.py
+++ CS50p/pset6/shirt/shirt.py
@@ -1,6 +1,6 @@
 import sys
 
-from PIL import Image
+from PIL import Image, ImageOps
 
 
 def main():
@@ -13,16 +13,42 @@ def main():
     output = sys.argv[2]
 
     # Check formats (.jpg .jpeg .png)
-    check_formats(input, output)
+    format_ok = check_formats(input, output)
+    if isinstance(format_ok, str):
+        sys.exit(format_ok)
+
+    # Open input file
+    try:
+        shirt_image = Image.open("shirt.png")
+        input_image = Image.open(input)
+        image = ImageOps.fit(input_image, size=(600, 600))
+        image.paste(shirt_image, (0, 0), mask=shirt_image)
+        image.save(output)
+
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+
+    sys.exit(0)
 
 
 def check_formats(input, output):
     valid_formats = [".jpg", ".jpeg", ".png"]
+    keep_going = True
+    # Check for invalid output extension
     for format in valid_formats:
-        if input.endswith(format):
+        if output.endswith(format):
+            keep_going = True
+            break
+        keep_going = False
+
+    if keep_going is False:
+        return "Invalid output"
+
+    # Check input and output different extensions
+    for format in valid_formats:
+        if input.endswith(format) and output.endswith(format):
             return True
-    
-    return False
+    return "Input and output have different extensions"
 
 
 def check_arguments(args):
