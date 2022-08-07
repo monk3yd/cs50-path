6dec717b9741156b54692472886bdf646ec499a4
monk3yd <jegajardog@gmail.com>
2022-08-07 03:46:47 -0300

    Finished project.

--- CS50p/pset7/watch/watch.py
+++ CS50p/pset7/watch/watch.py
@@ -2,9 +2,6 @@ import re
 
 
 def main():
-    # # test = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-    # test = '<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>'
-    # print(parse(test))
     print(parse(input("HTML: ")))
 
 
