f53f44062e4ddbe779b332d5124a323e7e127a7e
monk3yd <jegajardog@gmail.com>
2022-08-14 22:55:19 -0300

    Finished seasons problem set.

--- CS50p/pset8/seasons/seasons.py
+++ CS50p/pset8/seasons/seasons.py
@@ -9,14 +9,14 @@ def main():
     # prompt for date of birth
     bday_str = check_format(input("Date of Birth: "))
     # bday_str = "1999-01-01"
+    if bday_str is False:
+        sys.exit(1)
 
     # convert str date to datetime date
     bday = datetime.strptime(bday_str, "%Y-%m-%d").date()
 
     # todays date
     today = date.today()
-    # today_str = "2000-01-01"
-    # today = datetime.strptime(today_str, "%Y-%m-%d").date()
 
     # delta between today and bday
     delta = today - bday
@@ -25,20 +25,23 @@ def main():
     delta_minutes = int(delta.total_seconds() / 60)
 
     # convert age in minutes to words,
-    inf = inflect.engine()
-    delta_words = inf.number_to_words(delta_minutes)
-    text = delta_words.capitalize()
-    answer = re.sub(r"\sand\s", " ", text)
-    print(f"{answer} minutes")
+    minutes_words = convert_minutes_to_words(delta_minutes)
+    print(f"{minutes_words} minutes")
 
     sys.exit(0)
 
 
 def check_format(input):
     if matches := re.search(r"^\d{4}-\d{2}-\d{2}$", input):
-        # print(matches)
         return input
-    sys.exit(1)
+    return False
+
+
+def convert_minutes_to_words(minutes):
+    inf = inflect.engine()
+    delta_words = inf.number_to_words(minutes)
+    text = delta_words.capitalize()
+    return re.sub(r"\sand\s", " ", text)
 
 
 if __name__ == "__main__":
--- CS50p/pset8/seasons/test_seasons.py
+++ CS50p/pset8/seasons/test_seasons.py
@@ -1,5 +1,11 @@
-from season import check_format
+from seasons import check_format, convert_minutes_to_words
 
 
 def test_check_format():
-    assert check_format() ==
+    assert check_format("1999-01-01") == "1999-01-01"
+    assert check_format("2000-01-01") == "2000-01-01"
+    assert check_format("01-02-1993") is False
+
+
+def test_convert_minutes_to_words():
+    assert convert_minutes_to_words(525600) == "Five hundred twenty-five thousand, six hundred"
